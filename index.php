5d53f5ca68670f6f89d5c0ba75abfc9a3874ef26
majiy4 <riyuuko98@gmail.com>
2025-10-08 21:32:48 +0700

    Update index

--- index.php
+++ index.php
@@ -1,26 +1,3 @@
-<?php
-session_start();
-include 'koneksi.php';
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    // Query cek user
-    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-    $result = mysqli_query($conn, $sql);
-
-    if (mysqli_num_rows($result) > 0) {
-        $_SESSION['username'] = $username;
-        header("Location: dashboard.php");
-        exit();
-    } else {
-        echo "<script>alert('Username atau password salah!'); window.location.href='index.php';</script>";
-    }
-}
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
