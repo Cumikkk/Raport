b096019d96b21934c9709ac8c359c2e9565c4fd6
majiy4 <riyuuko98@gmail.com>
2025-10-09 19:04:04 +0700

    update includes untuk header dan navbar biar konsisten

---
+++ includes/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <title>Dashboard</title>
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
+  <link rel="stylesheet" href="/Raport/assets/css/dashboard.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <!-- <style>
+    .content {
+      margin-left: 260px;
+      padding: 25px;
+      
+      min-height: 100vh;
+    }
+    .card {
+      border-radius: 12px;
+      box-shadow: 0 2px 6px rgba(0,0,0,0.1);
+    }
+    .form-label { font-weight: 500; }
+    .logo-preview {
+      width: 150px;
+      height: 150px;
+      object-fit: contain;
+      border: 1px solid #ddd;
+      border-radius: 10px;
+      background-color: #fff;
+      padding: 10px;
+    }
+    .content h4 {
+      font-weight: bold;
+      margin-bottom: 25px;
+    }
+  </style> -->
+</head>
+<body>
+
